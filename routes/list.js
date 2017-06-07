f3eeda631be24c99a3f214b3b12d6b0038123608
cs-sakuragi <atsushi.sakuragi@central-soft.co.jp>
2017-06-07 21:53:35 +0900

    MYSQL対応

--- routes/list.js
+++ routes/list.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var connection = require('../model/mysqlConnection'); 
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('list', {title : '一覧'});
+	var query = 'SELECT EMPLOYEE_NO FROM MST_EMPLOYEE_BASE';
+
+	connection.query(query, function(err, rows) {
+		Console.log("aaa");
+		res.render('list',
+		{
+			title: '一覧画面',
+			testItem: rows
+		});
+	});
 });
 
 module.exports = router;
