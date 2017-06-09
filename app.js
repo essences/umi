cb55c2c788c9c7efc5fef71b99fbf5e152601fa1
Unknown <atsushi.sakuragi@central-soft.co.jp>
2017-06-09 10:50:54 +0900

    詳細画面追加

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var list = require('./routes/list');
+var detail = require('./routes/detail');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/list', list);
+app.use('/detail', detail);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/umi_search_list.js
+++ public/javascripts/umi_search_list.js
@@ -16,7 +16,7 @@ $(function() {
 
 	$('.detail-button').click(function(){
 		// 詳細画面を立ち上げる
-		window.open('detail.html?shainNo='+this.dataset.key, 'detailWindow'+this.dataset.key, 'width=800, height=700, scrollbars=yes');
+		window.open('detail?shainNo='+this.dataset.key, 'detailWindow'+this.dataset.key, 'width=800, height=700, scrollbars=yes');
 	});
 	
 });
