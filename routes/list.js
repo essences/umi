a149fa338c940bf8f09cb27f3ede95d196b25607
Unknown <h.konishi@central-soft.co.jp>
2017-08-26 18:07:04 +0900

    /list_sql: Auto stash before merge of "feature/list_sql" and "origin/feature/list_sql"
    一覧SQL見直し

--- routes/list.js
+++ routes/list.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	console.log("【INFO】list start");
 	console.dir(req);
-	
+
 	// 検索条件が指定されないときは、空配列を画面に返却する
 	if(!req.searchJoken) {
 		res.render('list',
@@ -18,40 +18,45 @@ router.get('/', function(req, res, next) {
 	}
 
 	var query = 'SELECT EMPLOYEE_NO FROM MST_EMPLOYEE_BASE';
-	
-// //	一覧画面項目を取るSQL
-// //	select
-// //	lpad(BASE.EMPLOYEE_NO, 5, '0') as EMPLOYEE_NO,
-// //	BASE.EMPLOYEE_FAMILY_NAME,
-// //	BASE.EMPLOYEE_FIRST_NAME,
-// //	DEPT.DEPT_NAME,
-// //	POSITION_UP.POSITION,
-// //	CLIENT.CLIENT_NAME,
-// //	WORK.WORK_PLACE_NAME,
-// //	PERSONAL.CELL_TEL_NO,
-// //	PERSONAL.WORKING_TEL_NO
-// //	from
-// //	MST_EMPLOYEE_BASE BASE
-// //	INNER JOIN MST_DEPT DEPT
-// //	on BASE.DEPT_CD = DEPT.DEPT_CD
-// //	LEFT OUTER JOIN
-//		(
-//			select *
-//			from
-//			TRN_POSITION_UPGRADE TMP_POSITION_UP
-//			where
-//			BASE.EMPLOYEE_NO = TMP_POSITION_UP.EMPLOYEE_NO
-//			order by TMP_POSITION_UP.UPGRADE_DATE desc
-//			limit 1
-//		) POSITION_UP
-// //	on BASE.EMPLOYEE_NO = POSITION_UP.EMPLOYEE_NO
-// //	INNER JOIN MST_CLIENT CLIENT
-// //	on BASE.CLIENT_CD = CLIENT.CLIENT_CD
-// //	INNER JOIN MST_WORK_PLACE WORK
-// //	on BASE.CLIENT_CD = WORK.CLIENT_CD
-// //	and BASE.WORK_PLACE_CD = WORK.WORK_PLACE_CD
-// // //	INNER JOIN MST_EMPLOYEE_PERSONAL PERSONAL
-// //	on BASE.EMPLOYEE_NO = PERSONAL.EMPLOYEE_NO
+
+ //	一覧画面項目を取るSQL
+/**
+ 	select
+ 	lpad(BASE.EMPLOYEE_NO, 5, '0') as EMPLOYEE_NO,
+ 	BASE.EMPLOYEE_FAMILY_NAME,
+ 	BASE.EMPLOYEE_FIRST_NAME,
+ 	DEPT.DEPT_NAME,
+ 	POSITION_UP.POSITION,
+ 	CLIENT.CLIENT_NAME,
+ 	WORK.WORK_PLACE_NAME,
+ 	PERSONAL.CELL_TEL_NO,
+ 	PERSONAL.WORKING_TEL_NO
+ 	from
+ 	MST_EMPLOYEE_BASE BASE
+ 	INNER JOIN MST_DEPT DEPT
+ 	on BASE.DEPT_CD = DEPT.DEPT_CD
+ 	LEFT OUTER JOIN
+		(
+			select
+			POSITION_UP.*
+			from
+			MST_EMPLOYEE_BASE BASE,
+			TRN_POSITION_UPGRADE POSITION_UP
+			where
+			BASE.EMPLOYEE_NO = 1
+			and BASE.EMPLOYEE_NO = POSITION_UP.EMPLOYEE_NO
+			order by POSITION_UP.UPGRADE_DATE desc
+			limit 1
+		) POSITION_UP
+	on BASE.EMPLOYEE_NO = POSITION_UP.EMPLOYEE_NO
+	INNER JOIN MST_CLIENT CLIENT
+	on BASE.CLIENT_CD = CLIENT.CLIENT_CD
+	INNER JOIN MST_WORK_PLACE WORK
+	on BASE.CLIENT_CD = WORK.CLIENT_CD
+	and BASE.WORK_PLACE_CD = WORK.WORK_PLACE_CD
+	INNER JOIN MST_EMPLOYEE_PERSONAL PERSONAL
+	on BASE.EMPLOYEE_NO = PERSONAL.EMPLOYEE_NO
+*/
 
 //	var whereStr = "where ";
 //	var searchJokenArr = req.searchJoken.split(" ");
@@ -86,7 +91,6 @@ router.get('/', function(req, res, next) {
 
 
 	connection.query(query, function(err, rows) {
-		console.dir(rows[0]);
 		res.render('list',
 		{
 			title: '一覧画面',
