4ab95e427ad43a9e6f47bb9f00a2830a6e521e48
koni1212 <h.konishi@central-soft.co.jp>
2017-09-04 22:15:23 +0900

    検索タイプのプルダウンにname付与

--- public/javascripts/umi_search_list.js
+++ public/javascripts/umi_search_list.js
@@ -1,15 +1,5 @@
 $(function() {
 
-	$('#searchType-name').change(function(){
-		// 検索タイプを設定する
-		$('#searchType').val(this.value);
-	});
-
-	$('#searchButton').click(function(){
-		// 検索ボタン押下時の処理
-		$('#searchType-name').change();
-	});
-
 	$('.detail-button').click(function(){
 		// 詳細画面を立ち上げる
 		window.open('detail?shainNo='+this.dataset.key, 'detailWindow'+this.dataset.key, 'width=800, height=700, scrollbars=yes');
