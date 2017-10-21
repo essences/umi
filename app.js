3cae221bfed7529397720dc406be9398bbdeae97
koni1212 <h.konishi@central-soft.co.jp>
2017-10-21 20:03:36 +0900

    意見箱の設置

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var menu = require('./routes/menu');
 var setauth = require('./routes/setauth');
 var list = require('./routes/list');
 var detail = require('./routes/detail');
+var comment = require('./routes/comment');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use('/menu', menu);
 app.use('/setauth', setauth);
 app.use('/list', list);
 app.use('/detail', detail);
+app.use('/comment', comment);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
