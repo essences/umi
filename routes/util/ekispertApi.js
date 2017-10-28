717baa342662e75ba1ea6919b5c745e7fd67e1fb
koni1212 <h.konishi@central-soft.co.jp>
2017-10-28 19:23:21 +0900

    ルートURL表示の一時的非表示化

--- routes/util/ekispertApi.js
+++ routes/util/ekispertApi.js
@@ -2,6 +2,11 @@ var env = require('../../../env.js');
 
 exports.getRouteUrl = function(from, to) {
 
+	// 最寄り駅をコード体系で保持する課題あるため、リンクは出さないようにして一度リリースする
+	if (true) {
+		return null;
+	}
+
 	if (from == null || to == null || from == "-" || to == "-") {
 		return null;
 	}
