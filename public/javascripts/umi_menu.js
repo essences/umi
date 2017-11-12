f1654ffe76216cbf5148ecc68afbc3877560bdbf
koni1212 <h.konishi@central-soft.co.jp>
2017-11-13 02:10:50 +0900

    国家資格マスタ更新処理

--- public/javascripts/umi_menu.js
+++ public/javascripts/umi_menu.js
@@ -14,6 +14,11 @@ $(function() {
 		$('#writeForm').submit();
 	});
 
+	$('#mstqualifyButton').click(function(){
+		$('#writeForm').attr('action', '/mstqualify');
+		$('#writeForm').submit();
+	});
+
 	$('#logoutButton').click(function(){
 		$('#logoutForm').attr('action', '/logout');
 		$('#logoutForm').submit();
