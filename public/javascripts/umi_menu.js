5cda7a60da45919abb4d7f166a559ea4ac85a8ac
koni1212 <h.konishi@central-soft.co.jp>
2017-11-23 21:11:27 +0900

    資格登録画面作成中

--- public/javascripts/umi_menu.js
+++ public/javascripts/umi_menu.js
@@ -34,6 +34,11 @@ $(function() {
 		$('#writeForm').submit();
 	});
 
+	$('#trnqualifyButton').click(function(){
+		$('#writeForm').attr('action', '/trnqualify');
+		$('#writeForm').submit();
+	});
+
 	$('#logoutButton').click(function(){
 		$('#logoutForm').attr('action', '/logout');
 		$('#logoutForm').submit();
