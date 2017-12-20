cb9a46e736ff7d6526ad02436d08a3202bb2fd69
cs-sakuragi <atsushi.sakuragi@central-soft.co.jp>
2017-12-20 22:42:19 +0900

    Update list.js

--- routes/list.js
+++ routes/list.js
@@ -100,10 +100,10 @@ router.get('/', function(req, res, next) {
 		}
 	} else if (req.query.searchType == '04') {
 		// 部署で検索
-		tmpWhereStr = "DEPT.DEPT_NAME like '%" + searchJokenArr[0] + "%' ";
+		tmpWhereStr = "DEPT.DEPT_NAME like '" + searchJokenArr[0] + "%' ";
 	} else if (req.query.searchType == '05') {
 		// 出身校で検索
-		tmpWhereStr = "EDU.SCHOOL like '%" + searchJokenArr[0] + "%' ";
+		tmpWhereStr = "EDU.SCHOOL like '" + searchJokenArr[0] + "%' ";
 	} else if (req.query.searchType == '06') {
 		// 退職年で検索
 		tmpWhereStr = "BASE.RETIREMENT_DATE between STR_TO_DATE('" + req.query.searchJoken + "0101', '%Y%m%d') and STR_TO_DATE('" + req.query.searchJoken + "1231', '%Y%m%d') ";
