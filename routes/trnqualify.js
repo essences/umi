4a63a3fa427ddec67dc04f1cdbb03a8b73ca4be9
Unknown <h.konishi@central-soft.co.jp>
2018-01-10 22:05:24 +0900

    資格情報登録画面の初期表示処理追加

--- routes/trnqualify.js
+++ routes/trnqualify.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pool = require('../model/mysqlConnection');
 
 var router = express.Router();
 
@@ -15,11 +16,76 @@ router.get('/', function(req, res, next) {
 		return;
 	}
 
-	res.render('trnqualify', {
-		query: req.query,
-		result: {}
+	// 国家資格マスタから一覧を取得
+	var searchNationalQualifyQuery = "select qualification_cd, qualify_name from umi_db.mst_national_qualify order by `order` asc ";
+	pool.getConnection(function(err, connection){
+		try {
+			connection.query(searchNationalQualifyQuery, function(err, rows) {
+				// エラー発生時はエラーハンドラをコールバックする
+				if (err) {
+					return next(err);
+				}
+				res.render('trnqualify', {
+					query: req.body,
+					result: {'qualify': rows}
+				});
+				return;
+			});
+		} finally {
+			connection.release();
+		}
 	});
-	return;
+});
+
+/**
+ * 社員Noから名前を返却する
+ */
+router.post('/confirm', function(req, res, next) {
+
+	var shainNo = req.body.shainNo;
+
+	// 国家資格マスタから一覧を取得
+	var searchNationalQualifyQuery = "select qualification_cd, qualify_name from umi_db.mst_national_qualify order by `order` asc ";
+	pool.getConnection(function(err, connection){
+		try {
+			connection.query(searchNationalQualifyQuery, function(err, rowsQualify) {
+				// エラー発生時はエラーハンドラをコールバックする
+				if (err) {
+					return next(err);
+				}
+
+				var searchNameQuery = "select employee_family_name, employee_first_name from mst_employee_base where employee_no = ? ";
+				connection.query(searchNameQuery, [shainNo], function(err, rowsName) {
+					// エラー発生時はエラーハンドラをコールバックする
+					if (err) {
+						return next(err);
+					}
+					if (rowsName.length == 1) {
+						res.render('trnqualify', {
+							query: req.body,
+							result: {
+								'qualify': rowsQualify,
+								'name': rowsName[0].employee_family_name + " " + rowsName[0].employee_first_name,
+							}
+						});
+						return;
+					} else {
+						var err = "社員Noが存在しません。";
+						res.render('trnqualify', {
+							query: req.body,
+							result: {
+								'qualify': rowsQualify,
+								'err': err
+							}
+						});
+						return;
+					}
+				});
+			});
+		} finally {
+			connection.release();
+		}
+	});
 });
 
 module.exports = router;
