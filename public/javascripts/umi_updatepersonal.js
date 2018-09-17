d008c49774938b8a327de50f9d70dc40fc1bcdfd
koni1212 <h.konishi@central-soft.co.jp>
2018-09-18 00:29:07 +0900

    社員情報変更の画面作成

--- public/javascripts/umi_updatepersonal.js
+++ public/javascripts/umi_updatepersonal.js
@@ -1,11 +1,68 @@
 $(function() {
 
+	// 必須項目マークを付与
+	$('label.require').before("*");
+
 	// 名前確認設定
 	$('#confirmButton').click(function(){
-		$('#updatepersonalForm').attr('action', '/updatepersonal/confirm');
-		$('#addpositionForm').submit();
+		$('#updatepersonalConfirmForm').attr('action', '/updatepersonal/confirm');
+		$('#updatepersonalConfirmForm').submit();
 	});
 
+	// 名前変更入力項目
+	$nameObj = $('#updatepersonalNameForm');
+	// 住所・電話番号変更入力項目
+	$addressObj = $('#updatepersonalAddressForm');
+
+	// 名前の入力チェックイベント追加
+	checkEvent($nameObj);
+	// 住所・電話番号の入力チェックイベント追加
+	checkEvent($addressObj);
+
+	// その他イベント追加
+	insertEvent($addressObj);
+
+	// 名前変更ボタン押下
+	$('#registNameButton').click(function() {
+
+		// 検索済みチェック
+		if ($('#employeeNo').val() == "") {
+			return false;
+		}
+
+		// エラー状態を解除する
+		clearError($nameObj.find('.input'));
+		clearError($addressObj.find('.input'));
+
+		// 入力フォームのフォーカスアウトイベントを起こす
+		$nameObj.find('.input').blur();
+		// エラー状態が残っているかのチェック
+		if ($nameObj.find('.input.error').length > 0) {
+			return false;
+		}
+
+	});
+
+	// 住所・電話番号変更ボタン押下
+	$('#registAddressButton').click(function() {
+
+		// 検索済みチェック
+		if ($('#employeeNo').val() == "") {
+			return false;
+		}
+
+		// エラー状態を解除する
+		clearError($nameObj.find('.input'));
+		clearError($addressObj.find('.input'));
+
+		// 入力フォームのフォーカスアウトイベントを起こす
+		$addressObj.find('.input').blur();
+		// エラー状態が残っているかのチェック
+		if ($addressObj.find('.input.error').length > 0) {
+			return false;
+		}
+
+	});
 });
 
 /**
@@ -27,4 +84,213 @@ function clearError($obj) {
 function dispError($obj, msg) {
 	$obj.after(`<span>${msg}</span>`)
 	$obj.addClass("error");
+}
+
+/**
+ * イベントを挿入する
+ * @param $obj
+ * @returns
+ */
+function insertEvent($obj) {
+
+	// 最寄り駅の入力補助
+	$($obj.find(':text[name="nearStationSupport"]')).change(function() {
+
+		// 最寄り駅のプルダウンを初期化する
+		$obj.find('select[name="nearStation"]').children().nextAll().remove();
+
+		// 最寄り駅の入力補助文字列を取得する
+		var support = $obj.find(':text[name="nearStationSupport"]').val();
+		if (support.length == 0) {
+			return;
+		}
+
+		// アクセスキーを取得する
+		var accessKey = $('#access-key').val();
+
+		$.ajax({
+			type : 'get',
+			url : 'http://api.ekispert.jp/v1/json/station/light?key=' + accessKey + '&name=' + support + '&type=train'
+		})
+		.then(
+				// 正常時の処理
+				function(data) {
+					var point = JSON.parse(data).ResultSet.Point;
+					if (point) {
+						var stationName;
+						var prefectureName;
+						if (point.length > 1) {
+							for (var i = 0; i < point.length; i++) {
+								stationName = point[i].Station.Name;
+								prefectureName = point[i].Prefecture.Name;
+								$obj.find('select[name="nearStation"]').append($('<option>').val(stationName).text(stationName + "　：　" + prefectureName));
+							}
+						} else {
+							stationName = point.Station.Name;
+							prefectureName = point.Prefecture.Name;
+							$obj.find('select[name="nearStation"]').append($('<option>').val(stationName).text(stationName + "　：　" + prefectureName));
+						}
+					}
+				},
+				// 異常時の処理
+				function() {
+					alert("何かしらの問題によりAPI連携に失敗しました");
+				}
+		);
+	});
+
+	// 初回1回だけChangeイベントを発生させる
+	$obj.find(':text[name="nearStationSupport"]').change();
+}
+
+/**
+ * 入力チェックする
+ * @param $clone
+ * @returns
+ */
+function checkEvent($obj) {
+	// 姓
+	checkEmployeeName($obj.find(':text[name="employeeFamilyName"]'));
+	// 名
+	checkEmployeeName($obj.find(':text[name="employeeFirstName"]'));
+	// セイ
+	checkEmployeeNameKana($obj.find(':text[name="employeeFamilyNameKana"]'));
+	// メイ
+	checkEmployeeNameKana($obj.find(':text[name="employeeFirstNameKana"]'));
+	// メールアドレス
+	checkEmail($obj.find(':text[name="email"]'));
+	// 郵便番号
+	checkZip($obj.find(':text[name="zip"]'));
+	// 住所
+	checkAddress($obj.find(':text[name="address"]'));
+	// 最寄り駅
+	checkSelect($obj.find('select[name="nearStation"]'));
+	// 電話番号
+	checkTelNo($obj.find(':text[name="telNo"]'));
+	// 携帯電話番号
+	checkTelNo($obj.find(':text[name="cellTelNo"]'));
+	// 郵便番号(緊急)
+	checkZip($obj.find(':text[name="zipHome"]'));
+	// 住所(緊急)
+	checkAddress($obj.find(':text[name="addressHome"]'));
+	// 電話番号(緊急)
+	checkTelNo($obj.find(':text[name="telNoHome"]'));
+}
+
+/**
+ * プルダウン項目の入力チェック
+ * @param $obj
+ * @returns
+ */
+function checkSelect($obj) {
+	$obj.on('blur', function() {
+		clearError($obj);
+		if ($obj.hasClass("require") && $obj.val() == "") {
+			dispError($obj, "選択してください");
+		}
+	});
+}
+
+/**
+ * 姓と名の入力チェック
+ * @param $obj
+ * @returns
+ */
+function checkEmployeeName($obj) {
+	$obj.on('blur', function() {
+		clearError($obj);
+		if ($obj.hasClass("require") && $obj.val() == "") {
+			dispError($obj, "入力してください");
+		} else if ($obj.val().length > 20) {
+			dispError($obj, "20文字以内で入力してください");
+		}
+	});
+}
+
+
+
+/**
+ * セイとメイの入力チェック
+ * @param $obj
+ * @returns
+ */
+function checkEmployeeNameKana($obj) {
+	$obj.on('blur', function() {
+		clearError($obj);
+		if ($obj.hasClass("require") && $obj.val() == "") {
+			dispError($obj, "入力してください");
+		} else if ($obj.val().length > 20) {
+			dispError($obj, "20文字以内で入力してください");
+		} else if (!$obj.val().match(/^[ァ-ロワヲンー]*$/)) {
+			dispError($obj, "全角カタカナで入力してください");
+		}
+	});
+}
+
+/**
+ * メールアドレスの入力チェック
+ * @param $obj
+ * @returns
+ */
+function checkEmail($obj) {
+	$obj.on('blur', function() {
+		clearError($obj);
+		if ($obj.hasClass("require") && $obj.val() == "") {
+			dispError($obj, "入力してください");
+		} else if ($obj.val().length > 256) {
+			dispError($obj, "256文字以内で入力してください");
+		} else if (!$obj.val().match(/^(([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+){0,1}$/)) {
+			dispError($obj, "メールアドレスの書式で入力してください");
+		}
+	});
+}
+
+/**
+ * 郵便番号の入力チェック
+ * @param $obj
+ * @returns
+ */
+function checkZip($obj) {
+	$obj.on('blur', function() {
+		clearError($obj);
+		if ($obj.hasClass("require") && $obj.val() == "") {
+			dispError($obj, "入力してください");
+		} else if ($obj.val().length != 0 && $obj.val().length != 7) {
+			dispError($obj, "7桁で入力してください");
+		} else if (!$obj.val().match(/^[0-9]*$/)) {
+			dispError($obj, "数値で入力してください");
+		}
+	});
+}
+
+/**
+ * 住所の入力チェック
+ * @param $obj
+ * @returns
+ */
+function checkAddress($obj) {
+	$obj.on('blur', function() {
+		clearError($obj);
+		if ($obj.hasClass("require") && $obj.val() == "") {
+			dispError($obj, "入力してください");
+		} else if ($obj.val().length > 200) {
+			dispError($obj, "200文字以内で入力してください");
+		}
+	});
+}
+
+/**
+ * 電話番号の入力チェック
+ * @param $obj
+ * @returns
+ */
+function checkTelNo($obj) {
+	$obj.on('blur', function() {
+		clearError($obj);
+		if ($obj.hasClass("require") && $obj.val() == "") {
+			dispError($obj, "入力してください");
+		} else if (!$obj.val().match(/^(0[0-9]{9,10}){0,1}$/)) {
+			dispError($obj, "0から始まる10桁か11桁の数値で入力してください");
+		}
+	});
 }
--- routes/updatepersonal.js
+++ routes/updatepersonal.js
@@ -14,15 +14,330 @@ router.get('/', function(req, res, next) {
 	if (!author.authWritable(req, res)) {
 		return;
 	}
+	render(req, res, next, []);
+});
 
+/**
+ * 社員Noから名前を返却する
+ */
+router.post('/confirm', function(req, res, next) {
+
+	var shainNo = req.body.shainNo;
+	var hoge = {};
+	hoge.returnFlg = false;
+
+	Promise.resolve()
+		.then((result) => searchName(shainNo))
+		.then((result) => {
+			if (hoge.returnFlg) return;
+			hoge.shainName = result[0]
+			hoge.err = result[1];
+			if (hoge.err != null) {
+				render(req, res, next, hoge);
+			}
+		})
+		.then((result) => searchPersonal(shainNo))
+		.then((result) => {
+			if (hoge.returnFlg) return;
+
+			hoge.personalInfo = result[0];
+			hoge.err = result[1];
+			if (hoge.err != null) {
+				render(req, res, next, hoge);
+			}
+		})
+		.then((result) => render(req, res, next, hoge))
+		.catch(function(err) {
+			return next(err);
+		});
+});
+
+/**
+ * 名前を更新する
+ */
+router.post('/updateName', function(req, res, next) {
+
+	var employeeNo = req.body.employeeNo;
+	var employeeFamilyName = req.body.employeeFamilyName;
+	var employeeFirstName = req.body.employeeFirstName;
+	var employeeFamilyNameKana = req.body.employeeFamilyNameKana;
+	var employeeFirstNameKana = req.body.employeeFirstNameKana;
+	var email = req.body.email;
+
+	var hoge = {};
+	hoge.returnFlg = false;
+
+	Promise.resolve()
+		.then((result) => updateName(employeeNo, employeeFamilyName, employeeFirstName, employeeFamilyNameKana, employeeFirstNameKana, email))
+		.then((result) => {
+			if (hoge.returnFlg) return;
+			hoge.err = result;
+			if (hoge.err != null) {
+				render(req, res, next, hoge);
+			}
+		})
+		.then((result) => searchName(employeeNo))
+		.then((result) => {
+			if (hoge.returnFlg) return;
+			hoge.shainName = result[0];
+			hoge.err = result[1];
+			if (hoge.err != null) {
+				render(req, res, next, hoge);
+			}
+		})
+		.then((result) => searchPersonal(employeeNo))
+		.then((result) => {
+			if (hoge.returnFlg) return;
+			hoge.personalInfo = result[0];
+			hoge.err = result[1];
+		})
+		.then((result) => render(req, res, next, hoge))
+		.catch(function(err) {
+			return next(err);
+		});
+});
+
+/**
+ * 住所・電話番号を更新する
+ */
+router.post('/updateAddress', function(req, res, next) {
+
+	var employeeNo = req.body.employeeNo;
+	var zip = req.body.zip;
+	var address = req.body.address;
+	var nearStation = req.body.nearStation;
+	var telNo = req.body.telNo;
+	var cellTelNo = req.body.cellTelNo;
+	var zipHome = req.body.zipHome;
+	var addressHome = req.body.addressHome;
+	var telNoHome = req.body.telNoHome;
+
+	var hoge = {};
+	hoge.returnFlg = false;
+
+	Promise.resolve()
+		.then((result) => updateAddress(employeeNo, zip, address, nearStation, telNo, cellTelNo, zipHome, addressHome, telNoHome))
+		.then((result) => {
+			console.log(10);
+			if (hoge.returnFlg) return;
+			hoge.err = result;
+			if (hoge.err != null) {
+				render(req, res, next, hoge);
+			}
+		})
+		.then((result) => searchName(employeeNo))
+		.then((result) => {
+			console.log(20);
+			console.dir(result);
+			if (hoge.returnFlg) return;
+			hoge.shainName = result[0];
+			hoge.err = result[1];
+			if (hoge.err != null) {
+				console.log(25);
+				render(req, res, next, hoge);
+			}
+		})
+		.then((result) => searchPersonal(employeeNo))
+		.then((result) => {
+			console.log(30);
+			if (hoge.returnFlg) return;
+			hoge.personalInfo = result[0];
+			hoge.err = result[1];
+		})
+		.then((result) => render(req, res, next, hoge))
+		.catch(function(err) {
+			return next(err);
+		});
+})
+
+/**
+ * 画面表示する
+ * @param req
+ * @param res
+ * @param next
+ * @param hoge
+ */
+function render(req, res, next, hoge) {
+	if (hoge.personalInfo == null) hoge.personalInfo = [];
+	hoge.returnFlg = true;
 	res.render('updatepersonal', {
 		query: req.body,
 		result: {
-			'name': "",
-			'err': ""
+			'name': hoge.shainName,
+			'personalInfo': hoge.personalInfo,
+			'err': hoge.err,
+			'accessKey': env.ekispertApiAccesskey
 		}
 	});
+}
 
-});
+/**
+ * 社員Noから社員名を取得
+ * @param shainNo 社員No
+ */
+function searchName(shainNo) {
+	return new Promise((resolve, reject) => {
+		var searchNameQuery = "select employee_family_name, employee_first_name from mst_employee_base where employee_no = ? ";
+		pool.getConnection(function(err, connection) {
+			try {
+				connection.query(searchNameQuery, [shainNo], function(err, rowsName) {
+					if (err) {
+						reject(err);
+					}
+					if (rowsName.length == 1) {
+						resolve([rowsName[0].employee_family_name + " " + rowsName[0].employee_first_name, null]);
+					} else {
+						resolve([null, "社員Noが存在しません。"]);
+					}
+				});
+			} finally {
+				connection.release();
+			}
+		})
+	})
+};
+
+/**
+ * 社員Noから社員情報を取得
+ * @param shainNo 社員No
+ */
+function searchPersonal(shainNo) {
+	return new Promise((resolve, reject) => {
+		var searchPersonalQuery =
+			"select " +
+			"base.employee_no, " +
+			"base.employee_family_name, " +
+			"base.employee_first_name, " +
+			"base.employee_family_name_kana, " +
+			"base.employee_first_name_kana, " +
+			"base.email, " +
+			"personal.zip, " +
+			"personal.address, " +
+			"personal.near_station, " +
+			"personal.tel_no, " +
+			"personal.cell_tel_no, " +
+			"personal.zip_home, " +
+			"personal.address_home, " +
+			"personal.tel_no_home " +
+			"from " +
+			"mst_employee_base base " +
+			"inner join mst_employee_personal personal " +
+			"on base.employee_no = personal.employee_no " +
+			"where " +
+			"base.employee_no = ? ";
+
+		pool.getConnection(function(err, connection) {
+			try {
+				connection.query(searchPersonalQuery, [shainNo], function(err, rows) {
+					if (err) {
+						reject(err);
+					}
+					if (rows.length == 1) {
+						resolve([rows[0], null]);
+					} else {
+						resolve([null, "社員Noが存在しません。"]);
+					}
+				});
+			} finally {
+				connection.release();
+			}
+		})
+	})
+}
+
+/**
+ * 名前を更新する
+ * @param employeeNo
+ * @param employeeFamilyName
+ * @param employeeFirstName
+ * @param employeeFamilyNameKana
+ * @param employeeFirstNameKana
+ * @param email
+ */
+function updateName(employeeNo, employeeFamilyName, employeeFirstName, employeeFamilyNameKana, employeeFirstNameKana, email) {
+	var updateNameQuery =
+		"update mst_employee_base " +
+		"set " +
+		"employee_family_name = ?, " +
+		"employee_first_name = ?, " +
+		"employee_family_name_kana = ?, " +
+		"employee_first_name_kana = ?, " +
+		"email = ? "
+		"where " +
+		"employee_no = ? ";
+
+	return new Promise((resolve, reject) => {
+		pool.getConnection(function(err, connection){
+			try {
+				connection.query(updateNameQuery, [employeeFamilyName, employeeFirstName, employeeFamilyNameKana, employeeFirstNameKana, email, employeeNo], function(err, result) {
+					if (err) {
+						reject(err);
+					}
+					connection.commit(function(err) {
+						if (err) {
+							connection.rollback(function() {
+								reject(err);
+							})
+						}
+						resolve();
+					})
+				});
+			} finally {
+				connection.release();
+			}
+		});
+	});
+}
+
+/**
+ * 住所・電話番号を更新する
+ * @param employeeNo
+ * @param zip
+ * @param address
+ * @param nearStation
+ * @param telNo
+ * @param cellTelNo
+ * @param zipHome
+ * @param addressHome
+ * @param telNoHome
+ * @returns
+ */
+function updateAddress(employeeNo, zip, address, nearStation, telNo, cellTelNo, zipHome, addressHome, telNoHome) {
+	var updateAddressQuery =
+		"update mst_employee_personal " +
+		"set " +
+		"zip = ?, " +
+		"address = ?, " +
+		"near_station = ?, " +
+		"tel_no = ?, " +
+		"cell_tel_no = ?, " +
+		"zip_home = ?, " +
+		"address_home = ?, " +
+		"tel_no_home = ? " +
+		"where " +
+		"employee_no = ? ";
+
+	return new Promise((resolve, reject) => {
+		pool.getConnection(function(err, connection){
+			try {
+				connection.query(updateAddressQuery, [zip, address, nearStation, telNo, cellTelNo, zipHome, addressHome, telNoHome, employeeNo], function(err, result) {
+					if (err) {
+						reject(err);
+					}
+					connection.commit(function(err) {
+						if (err) {
+							connection.rollback(function() {
+								reject(err);
+							})
+						}
+						resolve();
+					})
+				});
+			} finally {
+				connection.release();
+			}
+		});
+	});
+}
 
 module.exports = router;
