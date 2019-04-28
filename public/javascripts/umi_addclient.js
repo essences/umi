8bdb73de3f2e73e11a92045ec6791165eb8025e3
koni1212 <h.konishi@central-soft.co.jp>
2019-04-28 21:15:26 +0900

    契約先・常駐先の登録機能追加

--- public/javascripts/umi_addclient.js
+++ public/javascripts/umi_addclient.js
@@ -1,12 +1,32 @@
+
 $(function() {
+
+	var origWorkplaceTable = $("#workplaceTable").html();
+
+	checkEvent($("#addclientForm"));
+	checkEvent($("#addworkplaceForm"));
+
+	// 契約先一覧の選択
+	$("#clientTable td").click(function() {
+		$("#clientTable td").removeClass("selected");
+		$(this).addClass("selected");
+		$(this).siblings().addClass("selected");
+
+		// 選択状態の契約先コードを残しておく
+		$("#selectedClientCd").val($("#clientTable td.selected:first").html());
+
+		// 常駐先の検索、常駐先一覧の表示
+		var clientCd = $(this).parent().children("td:first").html();
+		$("#workplaceTable").html(origWorkplaceTable);
+		searchWorkplace(clientCd);
+
+	});
 
 	// 契約先を登録
 	$('#addClientButton').click(function(){
 
 		// エラー状態を解除する
 		clearError($('.addclient_frame').find('.input'));
-
-		checkEvent($('#addclientForm'));
 
 		// 入力フォームのフォーカスアウトイベントを起こす
 		$('.addclient_frame').find('.input').blur();
@@ -19,6 +39,76 @@ $(function() {
 		$('#addclientForm').attr('action', '/addclient/addclient');
 		$('#addclientForm').submit();
 	});
+
+	// 常駐先を登録
+	$("#addWorkplaceButton").click(function() {
+
+		// 選択済み契約先コードがない場合は何もしない
+		if ($("#selectedClientCd").val().length == 0) {
+			return false;
+		}
+
+		// エラー状態を解除する
+		clearError($(".addworkplace_frame").find(".input"));
+
+		// 入力フォームのフォーカスアウトイベントを起こす
+		$(".addworkplace_frame").find(".input").blur();
+
+		// エラー状態が残っているかのチェック
+		if ($(".addworkplace_frame").find(".input" + ".error").length > 0) {
+			return false;
+		}
+
+		$('#addworkplaceForm').attr('action', '/addclient/addworkplace');
+		$('#addworkplaceForm').submit();
+	});
+
+	// 最寄り駅の入力補助
+	$("#nearStationSupport").change(function() {
+
+		// 最寄り駅のプルダウンを初期化する
+		$("#nearStation").children().nextAll().remove();
+
+		// 最寄り駅の入力補助文字列を取得する
+		var support = $("#nearStationSupport").val();
+		if (support.length == 0) {
+			return;
+		}
+
+		// アクセスキーを取得する
+		var accessKey = $('#access-key').val();
+
+		$.ajax({
+			type : 'get',
+			url : 'http://api.ekispert.jp/v1/json/station/light?key=' + accessKey + '&name=' + support + '&type=train'
+		})
+		.then(
+				// 正常時の処理
+				function(data) {
+					var point = JSON.parse(data).ResultSet.Point;
+					if (point) {
+						var stationName;
+						var prefectureName;
+						if (point.length > 1) {
+							for (var i = 0; i < point.length; i++) {
+								stationName = point[i].Station.Name;
+								prefectureName = point[i].Prefecture.Name;
+								$("#nearStation").append($('<option>').val(stationName).text(stationName + "　：　" + prefectureName));
+							}
+						} else {
+							stationName = point.Station.Name;
+							prefectureName = point.Prefecture.Name;
+							$("#nearStation").append($('<option>').val(stationName).text(stationName + "　：　" + prefectureName));
+						}
+					}
+				},
+				// 異常時の処理
+				function() {
+					alert("何かしらの問題によりAPI連携に失敗しました");
+				}
+		);
+	});
+
 });
 
 /**
@@ -27,42 +117,50 @@ $(function() {
  * @returns
  */
 function checkEvent($obj) {
-	// 契約先CD
-	checkClientCd($obj.find('select[name="clientCd"]'));
+	// 契約先コード
+	checkCd($obj.find("input[name='clientCd']"), 4);
 	// 契約先名
-	checkClientName($obj.find('select[name="clientName"]'));
+	checkName($obj.find("input[name='clientName']"), 50);
+	// 常駐先コード
+	checkCd($obj.find("input[name='workplaceCd']"), 4);
+	// 常駐先名
+	checkName($obj.find("input[name='workplaceName']"), 50);
+	// 最寄り駅
+	checkSelect($obj.find("select[name='nearStation']"));
+	// 住所
+	checkName($obj.find("input[name='address']"), 100);
 }
 
 /**
- * 契約先CDの入力チェック
- * @param $obj
- * @returns
+ * 常駐先の検索
+ * @param clientCd
  */
-function checkClientCd($obj) {
-	$obj.on('blur', function() {
-		clearError($obj);
-		if ($obj.hasClass("require") && $obj.val() == "") {
-			dispError($obj, "入力してください");
-		} else if ($obj.val().length == 4) {
-			dispError($obj, "4文字で入力してください");
-		} else if (isNaN($obj.val())) {
-			dispError($obj, "数値で入力してください");
-		}
-	});
-}
+function searchWorkplace(clientCd) {
 
-/**
- * 契約先名の入力チェック
- * @param $obj
- * @returns
- */
-function checkClientName($obj) {
-	$obj.on('blur', function() {
-		clearError($obj);
-		if ($obj.hasClass("require") && $obj.val() == "") {
-			dispError($obj, "入力してください");
-		} else if ($obj.val().length <= 50) {
-			dispError($obj, "50文字以内で入力してください");
-		}
-	});
+	$.ajax({
+		async: true,
+		type : 'post',
+		url : '/addclient/searchWorkplace',
+		data: {"clientCd" : clientCd},
+		dataType: 'json'
+	})
+	.then(
+			// 正常時の処理
+			function(data) {
+				for (var i = 0; i < data.length; i++) {
+					$("#workplaceTable").append(
+							"<tr>" +
+							"<td>" + data[i].work_place_cd + "</td>" +
+							"<td>" + data[i].work_place_name + "</td>" +
+							"<td>" + data[i].near_station + "</td>" +
+							"<td>" + data[i].address + "</td>" +
+							"</tr>"
+							);
+				}
+			},
+			// 異常時の処理
+			function() {
+				console.log("常駐先検索に失敗しました。");
+			}
+	);
 }
--- routes/addclient.js
+++ routes/addclient.js
@@ -47,7 +47,17 @@ router.post('/addclient', function(req, res, next) {
 	var hoge = {};
 	hoge.returnFlg = false;
 
-	addClient(clientCd, clientName)
+	Promise.resolve()
+	.then((result) => searchClient())
+	.then((result) => {
+		if (hoge.returnFlg) return;
+		hoge.info = result[0];
+		hoge.err = result[1];
+		if (hoge.err != null) {
+			render(req, res, next, hoge);
+		}
+	})
+	.then((result) => addClient(clientCd, clientName))
 	.then((result) => {
 		if (hoge.returnFlg) return;
 		hoge.err = result;
@@ -73,6 +83,69 @@ router.post('/addclient', function(req, res, next) {
 });
 
 /**
+ * 常駐先を登録する
+ */
+router.post('/addworkplace', function(req, res, next) {
+
+	var clientCd = req.body.clientCd;
+	var workplaceCd = req.body.workplaceCd;
+	var workplaceName = req.body.workplaceName;
+	var nearStation = req.body.nearStation;
+	var address = req.body.address;
+
+	var hoge = {};
+	hoge.returnFlg = false;
+
+	Promise.resolve()
+	.then((result) => searchClient())
+	.then((result) => {
+		if (hoge.returnFlg) return;
+		hoge.info = result[0];
+		hoge.err = result[1];
+		if (hoge.err != null) {
+			render(req, res, next, hoge);
+		}
+	})
+	.then((result) => addWorkplace(clientCd, workplaceCd, workplaceName, nearStation, address))
+	.then((result) => {
+		if (hoge.returnFlg) return;
+		hoge.err = result;
+		if (hoge.err != null) {
+			render(req, res, next, hoge);
+		} else {
+			hoge.msg = "常駐先を登録しました。";
+		}
+	})
+	.then((result) => render(req, res, next, hoge))
+	.catch(function(err) {
+		return next(err);
+	});
+});
+
+/**
+ * 常駐先を取得する
+ */
+router.post('/searchWorkplace', function(req, res, next) {
+
+	var clientCd = req.body.clientCd;
+	var searchQuery = "select work_place_cd, work_place_name, near_station, address from mst_work_place where client_cd = ? ";
+	pool.getConnection(function(err, connection){
+		try {
+			connection.query(searchQuery, clientCd, function(err, rows) {
+				if (rows.length > 0) {
+					var rejson = JSON.stringify(rows);
+					res.send(rejson);
+				} else {
+					res.send("[]");
+				}
+			});
+		} finally {
+			connection.release();
+		}
+	});
+});
+
+/**
  * 画面表示する
  * @param req
  * @param res
@@ -83,13 +156,13 @@ function render(req, res, next, hoge) {
 	if (hoge.returnFlg) return;
 	if (hoge.info == null) hoge.info = [];
 	hoge.returnFlg = true;
-	console.dir(hoge.info);
 	res.render('addclient', {
 		query: req.body,
 		result: {
 			'info': hoge.info,
 			'err': hoge.err,
-			'msg': hoge.msg
+			'msg': hoge.msg,
+			'accessKey': env.ekispertApiAccesskey
 		}
 	});
 }
@@ -135,6 +208,43 @@ function addClient(clientCd, clientName) {
 					if (err) {
 						if (err.code = 'ER_DUP_ENRTY') {
 							resolve("すでにその契約先コードは登録済みです。やり直してください。");
+						}
+						reject(err);
+					}
+					connection.commit(function(err) {
+						if (err) {
+							connection.rollback(function() {
+								reject(err);
+							});
+						}
+						resolve();
+					});
+				});
+			} finally {
+				connection.release();
+			}
+		});
+	});
+}
+
+/**
+ * 常駐先を登録する
+ * @param clientCd
+ * @param workplaceCd
+ * @param workplaceName
+ * @param nearStation
+ * @param address
+ * @returns
+ */
+function addWorkplace(clientCd, workplaceCd, workplaceName, nearStation, address) {
+	return new Promise((resolve, reject) => {
+		var insertQuery = "insert into mst_work_place (client_cd, work_place_cd, work_place_name, near_station, address) values (?, ?, ?, ?, ?) ";
+		pool.getConnection(function(err, connection){
+			try {
+				connection.query(insertQuery, [clientCd, workplaceCd, workplaceName, nearStation, address], function(err, result) {
+					if (err) {
+						if (err.code = 'ER_DUP_ENRTY') {
+							resolve("すでにその常駐先コードは登録済みです。やり直してください。");
 						}
 						reject(err);
 					}
